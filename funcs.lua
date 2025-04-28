b2be2e7f1ac38e2b1881970a3df9179ce2804a6d
hersh2323 <stephenhercia@gmail.com>
2025-04-28 12:35:18 -0400

    created functions to simplify macro searching and creating

--- funcs.lua
+++ funcs.lua
@@ -1,12 +1,11 @@
 -- this file will be used for functions to be called by main.lua
-print("internals.lua has been executed.")
+print("funcs.lua has been executed.")
 
 local function iGetRemainingCharacterMacros()
 	local count = 0
 	-- since player macros are stored AFTER global macros, we must begin at macro index 121
 	local range_start = 121
 	local range_end = 120 + 30
-	
 	for i = range_start, range_end do
 		local current_macro = GetMacroInfo(i)
 		if current_macro ~= nil then
@@ -41,4 +40,43 @@ local function iGetRemainingGlobalMacros()
 	return range_end - count
 end
 
+local function bIsFreeCharacterMacroSlot()
+	if iGetRemainingCharacterMacros() > 0 then
+		return true
+	else
+		return false
+	end
+end
 
+
+local function bIsFreeGlobalMacroSlot()
+	if iGetRemainingGlobalMacros() > 0 then
+		return true
+	else
+		return false
+	end
+end
+
+
+local function bMacroExists(mname)
+	local macro_name = mname
+	local macro_found = false
+
+	for i = 1, 150 do
+    		local current_macro = GetMacroInfo(i)
+		if current_macro == macro_name then
+			return true
+		end
+	end    
+	return false
+end
+
+local function iGetMacroIndexByName(mname)
+	local macro_name = mname
+	for i = 1, 150 do
+    		local current_macro = GetMacroInfo(i)
+		if current_macro == macro_name then
+			return i
+		end
+	end
+end
